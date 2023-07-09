379a49b64890b257212fa8ab05c4e44909323f97
rustysamurai <chris@niwi.com.au>
2023-07-09 23:36:06 +1000

    Added jwt setup

---
+++ app/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "YFVOoL0qYKk0Gw1d6AEl0Ut9eE9PkgB2"
+  };
---
+++ app/middleware/index.js
@@ -0,0 +1,6 @@
+import authJwt from "./authjwt";
+import verifySignUp from "./verifySignup";
+export default {
+  authJwt,
+  verifySignUp
+};
